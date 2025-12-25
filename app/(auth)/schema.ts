c9674b21a5437c0f41252edcfd41fcbc46c7dc51
Dipika <maharjandipika49@gmail.com>
2025-12-25 20:17:19 +0545

    added zod validation for login

---
+++ app/(auth)/schema.ts
@@ -0,0 +1,25 @@
+import {email, z} from "zod";
+
+// Validates login form fields
+export const loginSchema = z.object({
+    email: z.string().email("Enter a valid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+//type inferred from loginSchema => automatically creates a ts type based on the schema
+export type LoginData = z.infer<typeof loginSchema>;
+
+// Validates registration form fields
+export const registerSchema = z.object({
+    email: z.string().email("Enter a valid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm Password must be at least 6 characters"),
+})
+//extra validation to check if password and confirmPassword match
+.refine((data) => data.password === data.confirmPassword,{
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+});
+
+//type inferred from registerSchema
+export type RegisterData = z.infer<typeof registerSchema>;
