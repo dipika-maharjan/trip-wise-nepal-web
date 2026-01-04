92aa1e28133fece391da05c170c0483479a7699b
Dipika <maharjandipika49@gmail.com>
2026-01-04 09:16:57 +0545

    setup MongoDB connection and user repository

---
+++ backend/src/database/mongodb.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { MONGODB_URI } from "../config/index";
+
+export async function connectDatabase(){
+    try{
+        await mongoose.connect(MONGODB_URI);
+        console.log("Connected to MongoDB");
+    }catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
