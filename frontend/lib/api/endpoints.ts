55cdc4cb9ca877318e6bb2e7ddd0124eb89d84de
Dipika <maharjandipika49@gmail.com>
2026-02-01 23:08:12 +0545

    update API configuration and endpoints

--- frontend/lib/api/endpoints.ts
+++ frontend/lib/api/endpoints.ts
@@ -1,8 +1,18 @@
 //backend route paths
 
 export const API = {
-    AUTH:{
-        REGISTER: "api/auth/register",  
-        LOGIN: "api/auth/login",
-    }
-}
+  AUTH: {
+    REGISTER: "api/auth/register",
+    LOGIN: "api/auth/login",
+    UPDATE_PROFILE: "api/auth", // PUT /api/auth/:id
+  },
+  ADMIN: {
+    USER: {
+      CREATE: "api/admin/users",
+      GET_ALL: "api/admin/users",
+      GET_BY_ID: "api/admin/users",
+      UPDATE: "api/admin/users", 
+      DELETE: "api/admin/users", 
+    },
+  },
+};
